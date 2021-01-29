be6c7340d68e7da591b43c68ba3dd6d33d6dc444
rubem.oliota <rubem.oliota@BNU-RUBEM>
2021-01-28 21:40:05 -0300

    002

--- montar_tabela.js
+++ montar_tabela.js
@@ -5,6 +5,57 @@ var salarios = [];
 
 $(function() {
       $("#planilha").hide();
+
+var colunas = [];
+      $.get(
+		"https://pw-mobile.firebaseio.com/item.json",
+		function(data) {
+            for(var key in data[0]){
+
+                colunas.push({ "mDataProp": key });
+
+                $("#tr_get").append("<th>"+key+"</th>");
+
+            }
+
+             var table = $('#tabelaGet').DataTable();
+    table.destroy();
+
+    $('#tabelaGet').dataTable({
+        "aaData": data,
+        "aoColumns":  colunas ,
+        language: {
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "_MENU_ resultados por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+
+
+            console.log(  colunas);
+		}
+	);
+
+
+
 });
 
 
